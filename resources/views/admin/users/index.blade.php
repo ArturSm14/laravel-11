8686d57d399ff1c80bfdbc2b0b11ca2c9f957935
Artsm14 <antonioartur123456@gmail.com.com>
2024-07-22 17:32:28 -0300

    :sparkles: feat: criação de templates

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -1,38 +1,33 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+@extends('admin.layouts.app')
+@section('title','Listagem dos Usuários')
+
+@section('content')
     <h1>Usuários</h1>
 
-    <a href="{{ route('users.create') }}">Novo</a>
+        <a href="{{ route('users.create') }}">Novo</a>
 
-    <table>
-        <thead>
-            <tr>
-                <th>Nome</th>
-                <th>E-mail</th>
-                <th>Ações</th>
-            </tr>
-        </thead>
-        <tbody>
-            @forelse ($users as $user)
+        <table>
+            <thead>
                 <tr>
-                    <td>{{ $user->name }}</td>
-                    <td>{{ $user->email }}</td>
-                    <td></td>
+                    <th>Nome</th>
+                    <th>E-mail</th>
+                    <th>Ações</th>
                 </tr>
-            @empty
-                <tr>
-                    <td colspan="100"> Nenhum usuário no banco</td>
-                </tr>
-            @endforelse
-        </tbody>
-    </table>
+            </thead>
+            <tbody>
+                @forelse ($users as $user)
+                    <tr>
+                        <td>{{ $user->name }}</td>
+                        <td>{{ $user->email }}</td>
+                        <td></td>
+                    </tr>
+                @empty
+                    <tr>
+                        <td colspan="100"> Nenhum usuário no banco</td>
+                    </tr>
+                @endforelse
+            </tbody>
+        </table>
 
-    {{ $users->links()}}
-</body>
-</html>
+        {{ $users->links()}}
+@endsection
